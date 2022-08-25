ccb5f5fb8d5abfc614a6e15d0688fd4035b05dcf
abdoul20 <abdoulkarimfoly1996@gmail.com>
2022-08-25 23:35:58 +0000

    installation de l'orm sequelize et suppression d'un pokemon

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express=require('express'); //permet de recuperer la dependance express
-const morgan=require('morgan');
-const favicon=require('serve-favicon');
-let pokemons=require('./mock-pokemon');
+const morgan=require('morgan');// c'est un middlewere nous permet logger les message avec 
+const favicon=require('serve-favicon');//le lien de favicon depuis le chemin
+const bodyParser=require('body-parser');//permet de transformer une chaine en json
+let pokemons=require('./mock-pokemon'); //la liste des pokemon
 const {success, getUniqueId}=require('./helper.js');
 const app=express();
 const port=3000;
@@ -13,18 +14,24 @@ app.use((req,res,nex)=>{
 peur etre remplacer par 
 app.use(morgan('dev'));
 */
+
 app
     .use(favicon(__dirname + '/favicon.ico'))
-    .use(morgan('dev')); //permet de creer des logger en mode dev
+    .use(morgan('dev')) //permet de creer des logger en mode dev
+    .use(bodyParser.json()) // nous permet de parser toutes les donnees entrant en sous format json
 
+
+
+/*La methode permettant de afficher un message dans le navigateur*/
 app.get('/',(req,res)=>res.send('hello, express js'));
 
+/*La methode permettant de retourner tous les pokemon*/
 app.get('/api/pokemons',(req,res)=>{
     //const pokemonTotal=pokemons.length
     const message='la liste des pokemons a bien ete retourner';
     res.json(success(message,pokemons));
 })
-
+/*La methode permettant de recuperer un pokemon*/
 app.get('/api/pokemons/:id',(req,res)=>{
     const id=parseInt(req.params.id);
     const pokemon=pokemons.find(pokemon=>pokemon.id===id);
@@ -41,6 +48,26 @@ app.post('/api/pokemons',(req,res)=>{
     pokemons.push(pokemonCreated);
     const message=`Le pokemon ${pokemonCreated.name} a ete creer avec success`;
     res.json(success(message,pokemonCreated));
+})
+
+/*La methode permettant de modifier un pokemon*/
+app.put('/api/pokemons/:id',(req,res)=>{
+    const id=parseInt(req.params.id);
+    const pokemonUpdated = {...req.body,id:id};
+    pokemons=pokemons.map(pokemon=>{ //nous permet de mettre la liste globale des pokemon en remplacant dans la liste le pokemon par sa valeur modifier
+        return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+    const message=`le pokemon ${pokemonUpdated.name} a ete modifier avec succees`;
+    res.json(success(message,pokemonUpdated));
+})
+
+/*La methode permettant de supprimer un pokemon*/
+app.delete('/api/pokemons/:id',(req,res)=>{
+    const id=parseInt(req.params.id);
+    pokemonDeleted=pokemons.find(pokemon => pokemon.id === id);
+    pokemons.filter(pokemon => pokemon.id !== id);//on retourne tous les pokemon sauf celui supprimer
+    const message=`le pokemon ${pokemonDeleted.name} a ete supprimer avec success`;
+    res.json(success(message,pokemonDeleted));
 })
 
 app.listen(port,()=>console.log(`notre application node est demarer sur le port: http://localhost:${port}`));
